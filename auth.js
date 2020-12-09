43e3ae831f909a06868a868afd38d912b63499ae
Vladimir Plesco <pleshco@deeplace.md>
2020-12-09 16:14:35 +0200

    Changed auth on conflicted branch

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 function authenticate(login, password) {
-	if (login === 'login' && password === 'password') {
+	if (login === 'login' || password === 'password') {
 		return 'You were logged!';
 	} else {
 		return 'login/password is incorrect';
